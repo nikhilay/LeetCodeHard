9a3f3df61ab255b89d24157f16a78b2134a7aeb7
nikhilay <nikhilayyagari15@gmail.com>
2017-01-26 22:44:12 -0500

    First Missing positive solution

---
+++ src/FirstMissingPositive.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Nikhil on 1/26/17.
+ */
+/**
+ * Given an unsorted integer array, find the first missing positive integer.
+ * For example,
+ * Given [1,2,0] return 3,
+ * and [3,4,-1,1] return 2.
+ * Your algorithm should run in O(n) time and uses constant space.
+ */
+
+/**
+ * Inspired from
+ * https://discuss.leetcode.com/topic/8293/my-short-c-solution-o-1-space-and-o-n-time/26
+ */
+public class FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[i];
+                nums[i] = nums[nums[i] - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length+1;
+    }
+}
